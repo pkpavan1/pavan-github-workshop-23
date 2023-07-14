cc8a871c29d0893a997609a594dc0c91ba620aae
pkpavan1 <136836487+pkpavan1@users.noreply.github.com>
2023-07-14 15:06:36 +0530

    inlcude stdio

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 #include<stdio.h>  //header file librery
 int main () {     
     printf("Hello world");  // printing in side double dash
-     return 0;   // it not work for this program
+     return 0;   // it not work for this progra\
 }
